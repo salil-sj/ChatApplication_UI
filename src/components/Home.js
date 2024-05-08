cae432b312e46ee8411b62aadbc4dc43b1b343ff
Salil Joshi <salilsj1619@gmail.com>
2024-05-08 23:43:36 +0530

    Profile Section added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "./SideBar";
 import ChatContacts from "./ChatContacts";
 import Header from "./Header";
@@ -6,21 +6,26 @@ import Chatbox from "./Chatbox";
 import ChatBoxHeader from "./ChatBoxHeader";
 import ChatContent from "./ChatContent";
 import { Outlet } from "react-router-dom";
+import Profile from "./Profile";
 
 const Home = () => {
+  const [isProfileOpen, setIsProfileOpen] = useState(true);
+
   return (
     <div>
       <Header />
+
       <div className="flex ">
         <div className="  ">
           <SideBar />
         </div>
+        <div>{isProfileOpen && <Profile />}</div>
+
         <div className="">
-        <Outlet/>
-         {/* <ChatContent/> */}
+          <Outlet />
+          {/* <ChatContent/> */}
         </div>
       </div>
-      
     </div>
   );
 };
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,63 @@
-import React from 'react'
-
-const Profile = () => {
+import React from "react";
+import { FaUserAlt } from "react-icons/fa";
+import { CiEdit } from "react-icons/ci";
+import { FaRegUser } from "react-icons/fa";
+import { RiShieldUserLine } from "react-icons/ri";
+import { MdOutlineEmail } from "react-icons/md";
+import { IoPhonePortraitOutline } from "react-icons/io5";
+import { MdOutlinePassword } from "react-icons/md";
+const Profile = ({ isOpen }) => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div
+      className={`fixed ml-1 top-[140px] h-full w-[28%] bg-gray-300 z-50 transition-transform duration-500  rounded-lg  shadow-lg ease-in-out `}
+    >
+      <div className="">
+        <div className="w-32 h-32 bg-gray-600 rounded-full flex justify-center  mt-6 ml-8">
+          <FaUserAlt className="text-gray-400 text-6xl my-auto" />
+        </div>
 
-export default Profile
+        <div className="ml-12 ">
+          <label className="cursor-pointer">
+            <span className="relative">
+              <input type="file" className="hidden" />
+              <span className=" px-4   text-gray-700 flex">
+                <CiEdit className="text-lg font-bold mt-1 " />
+                <h1 className="font-balsamiq mt-1 text-sm">Edit</h1>
+              </span>
+            </span>
+          </label>
+        </div>
+      </div>
+
+      <div className="ml-4 mt-6">
+        <div className="flex  my-4">
+          <RiShieldUserLine className="text-red-700" />
+          <h1 className="ml-2 font-balsamiq text-sm"> sjosh183</h1>
+        </div>
+
+        <div className="flex my-4">
+          <FaRegUser  className="text-green-700"/>
+          <h1 className="ml-2 font-balsamiq text-sm"> Salil Joshi</h1>
+        </div>
+
+        <div className="flex my-4">
+          <MdOutlineEmail className="text-blue-700"/>
+          <h1 className="ml-2 font-balsamiq text-sm"> salilsj1619@gmail.com</h1>
+        </div>
+
+        <div className="flex my-4">
+          <IoPhonePortraitOutline />
+          <h1 className="ml-2 font-balsamiq text-sm"> 8569858785</h1>
+        </div>
+
+        <div className="flex my-4">
+          <MdOutlinePassword />
+          <h1 className="ml-2 font-balsamiq text-sm"> **********</h1>
+        </div>
+      </div>
+      <button className="bg-red-800 hover:bg-red-900 m-2 text-white p-2 rounded-lg ml-4 w-[30%]">Logout</button>
+    </div>
+  );
+};
+
+export default Profile;
